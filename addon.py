3f6ce67a56f4a1b383c6b08fc308af390d846c3b
naoufelboukari <user@iMac-de-Naoufel.local>
2015-04-30 12:42:00 +0200

    update

--- addon.py
+++ addon.py
@@ -4,6 +4,6 @@ import xbmcgui
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-rtmpurl = 'http://46.245.160.224/mekke/mekke/chunklist_w581447799.m3u8 live=true'
+rtmpurl = 'http://aljazeera-doc-apple-live.adaptive.level3.net/apple/aljazeera/hq-doc/800kStream.m3u8 live=true'
  
 xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(rtmpurl)
