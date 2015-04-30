a1a005753161ca4add2a5f17da721c09b54813f6
naoufelboukari <user@iMac-de-Naoufel.local>
2015-04-30 12:05:49 +0200

    up

--- addon.py
+++ addon.py
@@ -4,6 +4,6 @@ import xbmcgui
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
-rtmpurl = 'rtmp://aljazeeraflashlivefs.fplive.net:1935/aljazeeraflashlive-live/aljazeera_doc_high live=true'
+rtmpurl = 'http://46.245.160.224/mekke/mekke/chunklist_w581447799.m3u8 live=true'
  
 xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(rtmpurl)
